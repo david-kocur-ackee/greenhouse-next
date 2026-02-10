60361f7017a6322ea9145c2c61169015527c91bf
David Kocur <david.kocur@ackee.cz>
2026-02-10 16:56:57 +0100

    Update login form structure.

---
+++ apps/web/src/modules/login/components/LoginForm/LoginForm.tsx
@@ -0,0 +1,68 @@
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm, type SubmitHandler } from 'react-hook-form';
+
+import { LoginFormSchema, type LoginFormSchemaType } from '~modules/form/hooks/LoginFormSchema';
+
+type LoginFormProps = {
+    onFormSubmit: SubmitHandler<LoginFormSchemaType>;
+};
+
+const LoginForm = ({ onFormSubmit }: LoginFormProps) => {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors, isSubmitting },
+    } = useForm<LoginFormSchemaType>({
+        resolver: zodResolver(LoginFormSchema),
+    });
+
+    return (
+        <div className='mt-10 sm:mx-auto sm:w-full sm:max-w-sm'>
+            <form onSubmit={handleSubmit(onFormSubmit)} className='space-y-6' method='POST'>
+                <div>
+                    <label htmlFor='email' className='block text-sm font-medium leading-6'>
+                        Email address
+                    </label>
+                    <div className='mt-2'>
+                        <input
+                            id='email'
+                            {...register('email')}
+                            autoComplete='email'
+                            className='block pl-3 w-full rounded-md border-0 py-1.5  shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-neon sm:text-sm sm:leading-6'
+                        />
+
+                        {errors.email && <p className='text-red-500'>{errors.email.message}</p>}
+                    </div>
+                </div>
+                <div>
+                    <div className='flex items-center justify-between'>
+                        <label htmlFor='password' className='block text-sm font-medium leading-6'>
+                            Password
+                        </label>
+                    </div>
+                    <div className='mt-2'>
+                        <input
+                            id='password'
+                            {...register('password')}
+                            autoComplete='current-password'
+                            className='block pl-3 w-full rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-neon sm:text-sm sm:leading-6'
+                        />
+                        {errors.password && <p className='text-red-500'>{errors.password.message}</p>}
+                    </div>
+                </div>
+
+                <div>
+                    <button
+                        type='submit'
+                        className='p-2 w-full h-full rounded-lg bg-dark text-white hover:bg-dark-light duration-100 grid items-center'
+                        disabled={isSubmitting}
+                    >
+                        Sign in
+                    </button>
+                </div>
+            </form>
+        </div>
+    );
+};
+
+export default LoginForm;
--- apps/web/src/modules/login/components/LoginPage/LoginPage.tsx
+++ apps/web/src/modules/login/components/LoginPage/LoginPage.tsx
@@ -1,4 +1,3 @@
-import { useState } from 'react';
 import { useRouter } from 'next/router';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm, type SubmitHandler } from 'react-hook-form';
@@ -7,6 +6,7 @@ import { FaCarrot } from 'react-icons/fa';
 import { routes } from '~constants/routes';
 import { LoginFormSchema, type LoginFormSchemaType } from '~modules/form/hooks/LoginFormSchema';
 
+import LoginForm from '../LoginForm/LoginForm';
 import useLogin from './hooks/useLogin';
 
 export default function Login() {
@@ -14,15 +14,7 @@ export default function Login() {
 
     const { mutate: login } = useLogin();
 
-    const {
-        register,
-        handleSubmit,
-        formState: { errors, isSubmitting },
-    } = useForm<LoginFormSchemaType>({
-        resolver: zodResolver(LoginFormSchema),
-    });
-
-    const onSubmit: SubmitHandler<LoginFormSchemaType> = data => {
+    const onFormSubmit: SubmitHandler<LoginFormSchemaType> = data => {
         login(data);
         router.push(routes.home);
     };
@@ -39,52 +31,7 @@ export default function Login() {
                     Sign in to your account
                 </h2>
             </div>
-
-            <div className='mt-10 sm:mx-auto sm:w-full sm:max-w-sm'>
-                <form onSubmit={handleSubmit(onSubmit)} className='space-y-6' method='POST'>
-                    <div>
-                        <label htmlFor='email' className='block text-sm font-medium leading-6'>
-                            Email address
-                        </label>
-                        <div className='mt-2'>
-                            <input
-                                id='email'
-                                {...register('email')}
-                                autoComplete='email'
-                                className='block pl-3 w-full rounded-md border-0 py-1.5  shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-neon sm:text-sm sm:leading-6'
-                            />
-
-                            {errors.email && <p className='text-red-500'>{errors.email.message}</p>}
-                        </div>
-                    </div>
-                    <div>
-                        <div className='flex items-center justify-between'>
-                            <label htmlFor='password' className='block text-sm font-medium leading-6'>
-                                Password
-                            </label>
-                        </div>
-                        <div className='mt-2'>
-                            <input
-                                id='password'
-                                {...register('password')}
-                                autoComplete='current-password'
-                                className='block pl-3 w-full rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-neon sm:text-sm sm:leading-6'
-                            />
-                            {errors.password && <p className='text-red-500'>{errors.password.message}</p>}
-                        </div>
-                    </div>
-
-                    <div>
-                        <button
-                            type='submit'
-                            className='p-2 w-full h-full rounded-lg bg-dark text-white hover:bg-dark-light duration-100 grid items-center'
-                            disabled={isSubmitting}
-                        >
-                            Sign in
-                        </button>
-                    </div>
-                </form>
-            </div>
+            <LoginForm onFormSubmit={onFormSubmit} />
         </div>
     );
 }
